b87daffb00e766d245214777b0689507a8e6db02
Alexander Schindler <schindal@in.tum.de>
2024-07-24 23:45:21 +0200

    Add more lsp funcitonality and code completions for nvim

--- neovim/dot-config/nvim/lua/plugins/lsp-config.lua
+++ neovim/dot-config/nvim/lua/plugins/lsp-config.lua
@@ -30,22 +30,22 @@ function Basic_setup_mlspconfig()
 end
 
 function Basic_setup_lsp()
-
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
     local lspconfig = require("lspconfig")
-    lspconfig.lua_ls.setup({})
-    lspconfig.typos_lsp.setup({})
-    lspconfig.bashls.setup({})
-    lspconfig.clangd.setup({})
-    lspconfig.neocmake.setup({})
-    lspconfig.dockerls.setup({})
-    lspconfig.docker_compose_language_service.setup({})
-    lspconfig.jsonls.setup({})
-    lspconfig.jdtls.setup({})
-    lspconfig.ltex.setup({})
-    lspconfig.autotools_ls.setup({})
-    lspconfig.pylsp.setup({})
-    lspconfig.rust_analyzer.setup({})
-    lspconfig.terraformls.setup({})
+    lspconfig.lua_ls.setup({ capabilities = capabilities })
+    lspconfig.typos_lsp.setup({ capabilities = capabilities })
+    lspconfig.bashls.setup({ capabilities = capabilities })
+    lspconfig.clangd.setup({ capabilities = capabilities })
+    lspconfig.neocmake.setup({ capabilities = capabilities })
+    lspconfig.dockerls.setup({ capabilities = capabilities })
+    lspconfig.docker_compose_language_service.setup({ capabilities = capabilities })
+    lspconfig.jsonls.setup({ capabilities = capabilities })
+    lspconfig.jdtls.setup({ capabilities = capabilities })
+    lspconfig.ltex.setup({ capabilities = capabilities })
+    lspconfig.autotools_ls.setup({ capabilities = capabilities })
+    lspconfig.pylsp.setup({ capabilities = capabilities })
+    lspconfig.rust_analyzer.setup({ capabilities = capabilities })
+    lspconfig.terraformls.setup({ capabilities = capabilities })
 
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
     vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
@@ -55,14 +55,17 @@ end
 return {
     {
         "williamboman/mason.nvim",
+        lazy = false,
         config = Basic_setup_mason,
     },
     {
         "williamboman/mason-lspconfig.nvim",
+        lazy = false,
         config = Basic_setup_mlspconfig,
     },
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = Basic_setup_lsp,
     },
 }
--- neovim/dot-config/nvim/lua/plugins/none-ls.lua
+++ neovim/dot-config/nvim/lua/plugins/none-ls.lua
@@ -7,8 +7,17 @@ function Basic_setup_null()
             null_ls.builtins.formatting.black,
             null_ls.builtins.formatting.isort,
             null_ls.builtins.formatting.clang_format,
+            null_ls.builtins.formatting.bibclean,
+            null_ls.builtins.formatting.terraform_fmt,
+            null_ls.builtins.formatting.usort,
             null_ls.builtins.diagnostics.pylint,
             null_ls.builtins.diagnostics.cppcheck,
+            null_ls.builtins.diagnostics.checkmake,
+            null_ls.builtins.diagnostics.checkstyle,
+            null_ls.builtins.diagnostics.commitlint,
+            null_ls.builtins.diagnostics.hadolint,
+            null_ls.builtins.diagnostics.terraform_validate,
+            null_ls.builtins.diagnostics.tfsec,
             null_ls.builtins.hover.printenv,
         }
     })
