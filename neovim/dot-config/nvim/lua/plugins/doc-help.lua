04ec4a906197aaea9a0aaa47551c197dba02794f
Alexander Schindler <alex.schindler@tum.de>
2026-08-31 22:25:02 +0200

    small neovim changes

--- neovim/dot-config/nvim/lua/plugins/doc-help.lua
+++
@@ -1,4 +0,0 @@
-return {
-    "AlexTUM/investigate.nvim",
-    config = true,
-}
--- neovim/dot-config/nvim/lua/vim-opts.lua
+++ neovim/dot-config/nvim/lua/vim-opts.lua
@@ -1,8 +1,9 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
-vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.breakindent = true
 vim.opt.pumblend = 10
