94c3e0dbf14f68e2129c9bd594d7b9f96a6a9ba8
Alexander Schindler <alex.schindler@tum.de>
2026-09-02 23:07:35 +0200

    descriptions for nvim keymaps

--- neovim/dot-config/nvim/lua/plugins/lsp-config.lua
+++ neovim/dot-config/nvim/lua/plugins/lsp-config.lua
@@ -2,130 +2,132 @@
 -- Requirements: npm, docker, cmake, gcc (others probably)
 
 local function setup_mason()
-
-    require("mason").setup({
-        max_concurrent_installers = 6,
-    })
-
+	require("mason").setup({
+		max_concurrent_installers = 6,
+	})
 end
 
 local function setup_mlspconfig()
-
-    require("mason-lspconfig").setup({
-        ensure_installed = {
-            "lua_ls",
-            "typos_lsp",
-            "bashls",
-            "clangd",
-            "neocmake",
-            "dockerls",
-            "docker_compose_language_service",
-            "jsonls",
-            "jdtls",
-            "ltex",
-            "autotools_ls",
-            "pylsp",
-            "rust_analyzer",
-            "terraformls",
-        },
-    })
-
+	require("mason-lspconfig").setup({
+		ensure_installed = {
+			"lua_ls",
+			"typos_lsp",
+			"bashls",
+			"clangd",
+			"neocmake",
+			"dockerls",
+			"docker_compose_language_service",
+			"jsonls",
+			"jdtls",
+			"ltex",
+			"autotools_ls",
+			"pylsp",
+			"rust_analyzer",
+			"terraformls",
+		},
+	})
 end
 
 local function setup_lsp()
-    local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    local servers = {
-        "lua_ls",
-        "typos_lsp",
-        "bashls",
-        "clangd",
-        "neocmake",
-        "dockerls",
-        "docker_compose_language_service",
-        "jsonls",
-        "jdtls",
-        "ltex",
-        "autotools_ls",
-        "pylsp",
-        "rust_analyzer",
-        "terraformls",
-    }
-    -- common options for all servers
-    for _, server in ipairs(servers) do
-        vim.lsp.config(server, {
-            capabilities = capabilities,
-        })
-    end
+	local servers = {
+		"lua_ls",
+		"typos_lsp",
+		"bashls",
+		"clangd",
+		"neocmake",
+		"dockerls",
+		"docker_compose_language_service",
+		"jsonls",
+		"jdtls",
+		"ltex",
+		"autotools_ls",
+		"pylsp",
+		"rust_analyzer",
+		"terraformls",
+	}
+	-- common options for all servers
+	for _, server in ipairs(servers) do
+		vim.lsp.config(server, {
+			capabilities = capabilities,
+		})
+	end
 
-    -- rustfmt via rust-analyzer
-    vim.lsp.config("rust_analyzer", {
-        capabilities = capabilities,
-        settings = {
-            ["rust-analyzer"] = {
-                rustfmt = {
-                    enable = true,
-                    -- overrideCommand = {
-                    --     "/usr/bin/rustfmt",
-                    --     "--emit",
-                    --     "stdout",
-                    -- },
-                },
-            },
-        },
-    })
+	-- rustfmt via rust-analyzer
+	vim.lsp.config("rust_analyzer", {
+		capabilities = capabilities,
+		settings = {
+			["rust-analyzer"] = {
+				rustfmt = {
+					enable = true,
+					-- overrideCommand = {
+					--     "/usr/bin/rustfmt",
+					--     "--emit",
+					--     "stdout",
+					-- },
+				},
+			},
+		},
+	})
 
-    -- latex specific options
-    vim.lsp.config("ltex", {
-        capabilities = capabilities,
-        cmd_env = {
-            JAVA_TOOL_OPTIONS = "-Djdk.xml.totalEntitySizeLimit=200000",
-        },
-        on_attach = function()
-            require("ltex_extra").setup({
-                load_langs = { "en-US" },
-            })
-        end,
-    })
+	-- latex specific options
+	vim.lsp.config("ltex", {
+		capabilities = capabilities,
+		cmd_env = {
+			JAVA_TOOL_OPTIONS = "-Djdk.xml.totalEntitySizeLimit=200000",
+		},
+		on_attach = function()
+			require("ltex_extra").setup({
+				load_langs = { "en-US" },
+			})
+		end,
+	})
 
-    -- enable servers
-    vim.lsp.enable(servers)
+	-- enable servers
+	vim.lsp.enable(servers)
 
-    -- Buffer-local keymaps are better installed when an LSP attaches.
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(event)
-            local opts = { buffer = event.buf, silent = true }
-
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-            vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-        end,
-    })
+	-- Buffer-local keymaps are better installed when an LSP attaches.
+	vim.api.nvim_create_autocmd("LspAttach", {
+		callback = function(event)
+			local buffer = event.buf
+			local function map(mode, lhs, rhs, desc)
+				vim.keymap.set(mode, lhs, rhs, {
+					buffer = buffer,
+					silent = true,
+					desc = "LSP: " .. desc,
+				})
+			end
+			map("n", "K", vim.lsp.buf.hover, "hover documentation")
+			map("n", "gd", vim.lsp.buf.definition, "Go to definition")
+			map({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, "Code action")
+			map("n", "<leader>rn", vim.lsp.buf.rename, "Rename symbol")
+		end,
+	})
 end
 
 return {
-    {
-        "barreiroleo/ltex_extra.nvim",
-        lazy = false,
-    },
-    {
-        "williamboman/mason.nvim",
-        lazy = false,
-        config = setup_mason,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        lazy = false,
-        config = setup_mlspconfig,
-    },
-    {
-        "neovim/nvim-lspconfig",
-        lazy = false,
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "barreiroleo/ltex_extra.nvim",
-            },
-        config = setup_lsp,
-    },
+	{
+		"barreiroleo/ltex_extra.nvim",
+		lazy = false,
+	},
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = setup_mason,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = setup_mlspconfig,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"barreiroleo/ltex_extra.nvim",
+		},
+		config = setup_lsp,
+	},
 }
--- neovim/dot-config/nvim/lua/plugins/venvs.lua
+++ neovim/dot-config/nvim/lua/plugins/venvs.lua
@@ -2,21 +2,21 @@
 -- keybinds to select
 
 return {
-    "linux-cultist/venv-selector.nvim",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "nvim-telescope/telescope.nvim",
-        "mfussenegger/nvim-dap-python",
-        "mfussenegger/nvim-dap",
-    },
-    opts = {
-        dap_enabled = true,
-        name = { "venv", ".venv" },
-    },
-    keys = {
-        { "<leader>vs", "<cmd>VenvSelect<cr>",       { desc = "Select Venv" } },
-        { "<leader>vc", "<cmd>VenvSelectCached<cr>", { desc = "Select Venv from Cache" } },
-    },
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"nvim-telescope/telescope.nvim",
+		"mfussenegger/nvim-dap-python",
+		"mfussenegger/nvim-dap",
+	},
+	opts = {
+		dap_enabled = true,
+		name = { "venv", ".venv" },
+	},
+	keys = {
+		{ "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Select Venv" },
+		{ "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Select Venv from Cache" },
+	},
 }
 
 -- One potential issue: lsp might not pick up the current venv after selection
